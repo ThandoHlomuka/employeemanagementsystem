f0d54faf905b95a8480ff907581788efc20028bc
Thando Hlomuka <90837977+ThandoHlomuka@users.noreply.github.com>
2024-12-21 18:01:31 +0200

    Update employeeRoutes.js

--- backend/src/routes/employeeRoutes.js
+++ backend/src/routes/employeeRoutes.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { createEmployee, getEmployees, getFilteredEmployees, updateEmployee, deleteEmployee } = require('../controllers/employeeController');
+const { createEmployee, getEmployees, getFilteredEmployees, getAllEmployees, updateEmployee, deleteEmployee } = require('../controllers/employeeController');
 const authMiddleware = require('../middleware/authMiddleware');
 const roleMiddleware = require('../middleware/roleMiddleware');
 const router = express.Router();
@@ -7,6 +7,7 @@ const router = express.Router();
 router.post('/', authMiddleware, roleMiddleware (['admin', 'executive', 'manager']), createEmployee);
 router.get('/', authMiddleware, getEmployees);
 router.get('/', authMiddleware, getFilteredEmployees);
+router.get('/all', authMiddleware, getAllEmployees);
 router.put('/:id' authMiddleware,  roleMiddleware (['admin', 'executive', 'manager']), updateEmployee);
 router.delete('/:id', authMiddleware,  roleMiddleware (['admin', 'executive', 'manager']) deleteEmployee);
 
