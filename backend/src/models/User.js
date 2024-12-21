618834557c0b0c02cdf5a6b67759099c32d59bd1
Thando Hlomuka <90837977+ThandoHlomuka@users.noreply.github.com>
2024-12-21 16:57:37 +0200

    Update User.js

--- backend/src/models/User.js
+++ backend/src/models/User.js
@@ -3,7 +3,9 @@ const mongoose = require('mongoose');
 const UserSchema = new mongoose.Schema({
     username: { type: String, required: true, unique: true },
     password: { type: String, required: true },
-    role: { type: String, default: 'employee' }
+    role: { type: String,
+           enum: ['admin', 'manager', 'employee' 'IT', 'Sales', 'executive', 'marketing', 'hr'],
+           default: 'employee' }
 });
 
 module.exports = mongoose.model('User ', UserSchema);
