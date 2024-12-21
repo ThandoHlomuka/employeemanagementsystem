4db6fb1f6a8de621b032098a10ab72bb2b520b6c
Thando Hlomuka <90837977+ThandoHlomuka@users.noreply.github.com>
2024-12-21 17:29:49 +0200

    Update Employee.js

--- backend/src/models/Employee.js
+++ backend/src/models/Employee.js
@@ -5,7 +5,9 @@ const EmployeeSchema = new mongoose.Schema({
     email: { type: String, required: true, unique: true },
     position: { type: String, required: true },
     department: { type: mongoose.Schema.Types.ObjectId, ref: 'Department' },
-    dateOfJoining: { type: Date, default: Date.now }
+    Division: { type: String, enum: ['sales', 'IT', 'marketing', 'executive', 'managerial', ], default: 'executive' },
+    dateOfJoining: { type: Date, default: Date.now },
+    status: { type: String, enum: ['active', 'inactive'], default: 'active' }
 });
 
 module.exports = mongoose.model('Employee', EmployeeSchema);
