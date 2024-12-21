f33b1ad886a257c9f7b2eff46af9b557f582e257
Thando Hlomuka <90837977+ThandoHlomuka@users.noreply.github.com>
2024-12-21 17:14:17 +0200

    Update employeeRoutes.js

--- backend/src/routes/employeeRoutes.js
+++ backend/src/routes/employeeRoutes.js
@@ -4,9 +4,9 @@ const authMiddleware = require('../middleware/authMiddleware');
 const roleMiddleware = require('../middleware/roleMiddleware');
 const router = express.Router();
 
-router.post('/', authMiddleware, roleMiddleware (['admin', 'manager']), createEmployee);
+router.post('/', authMiddleware, roleMiddleware (['admin', 'executive', 'manager']), createEmployee);
 router.get('/', authMiddleware, getEmployees);
-router.put('/:id' authMiddleware,  roleMiddleware (['admin', 'manager']), updateEmployee);
-router.delete('/:id', authMiddleware,  roleMiddleware (['admin', 'manager']) deleteEmployee);
+router.put('/:id' authMiddleware,  roleMiddleware (['admin', 'executive', 'manager']), updateEmployee);
+router.delete('/:id', authMiddleware,  roleMiddleware (['admin', 'executive', 'manager']) deleteEmployee);
 
 module.exports = router;
